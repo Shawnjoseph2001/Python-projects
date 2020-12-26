31766884d0a488e1ad4c7740fe2451d22b28b669
Shawnjoseph2001 <shawnjoseph2001@gmail.com>
2020-12-26 00:58:33 -0800

    ;

--- Example/test.py
+++ Example/test.py
@@ -1,1 +1,10 @@
-print("Hello")
+import numpy as np
+
+matrix = [[1, 2],
+          [3, 4],
+          [5, 6]]
+
+np_matrix = np.array(matrix)
+t_matrix = np_matrix.T
+
+print(t_matrix.shape[0] + np_matrix.shape[0])
